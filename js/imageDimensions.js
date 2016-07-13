2f90a15ac9dc6ab28b94ed4f1db0b8be2474f78d
beck410 <beck410@hotmail.com>
2016-07-12 22:17:27 -0500

    first four sections

---
+++ js/imageDimensions.js
@@ -0,0 +1,35 @@
+;(function(){
+    $(document).ready(function(){
+        setImageDimensions();
+    });
+
+    $(window).resize(function(){
+        setImageDimensions();
+    });
+
+    function setImageDimensions(){
+
+        var extraPixels = addExtraPixels(),
+            browserWidth = $(window).width(),
+            browserHeight = $(window).height() + extraPixels;
+
+        $('.section').each(function() {
+            if($(this).height() < browserHeight && browserWidth > 600) {
+                $(this).css('height', browserHeight);
+            } else {
+                $(this).css('height', 'auto');
+            }
+
+            $(this).css('width', browserWidth);
+        });
+    }
+
+    function addExtraPixels() {
+        if(navigator.userAgent.indexOf('iPhone') != -1) {
+          return 60;
+        }
+
+        return 0;
+    };
+
+})();
